687e19865bf4f40b070e6068bd24c5f4351ada35
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2022-07-15 22:17:50 +0300

    refactoring 98

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-val=$(find /usr/CLionProjects/ci-test/ -type f -name "*.rs" -printf '%s ' -exec basename \{} .rs \; | sort -k2)
+val=$(find . -type f -name "*.rs" -printf '%s ' -exec basename \{} .rs \; | sort -k2)
 echo "$val"
