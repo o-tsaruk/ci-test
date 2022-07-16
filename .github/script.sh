5cbb24d8def34c18590691435f7d4fa9eae688bf
o-tsaruk <oleksandr.v.tsaruk@gmail.com>
2022-07-16 19:43:50 +0300

    refactoring 105

--- .github/script.sh
+++ .github/script.sh
@@ -2,12 +2,3 @@
 
 val=$(find . -type f -name "*.rs" -printf '%s ' -exec basename \{} .rs \; | sort -k2)
 echo "$val"
-
-MY_STRING=$(cat << EOF
-| Size | Name |
-| --- | --- |
-EOF
-)
-
-cat "$MY_STRING"
-export MY_STRING
